45c8288cdf6331bc4a6a0a57314e1d5f5ea182f3
Diegofabri <62312511+Diegofabri@users.noreply.github.com>
2021-11-24 23:16:17 -0300

    Add files via upload

---
+++ Atividade 17/PWEBNode/ExerciciosAnteriores/exercicio7.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var app=express(); // executando o express
+
+app.listen(3000, function(){
+
+               console.log("servidor com express foi carregado");
+
+});
---
+++ Atividade 17/PWEBNode/ExerciciosAnteriores/segundoS.js
@@ -0,0 +1,11 @@
+console.log('1');
+
+t();
+
+console.log('3');
+
+function t() {
+
+  console.log('2');
+
+}
